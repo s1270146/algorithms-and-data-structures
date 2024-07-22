efa4e0aabb5d834fd3ce37b1e229298287d11e9a
Koyo Mori <s1270146@gmail.com>
2024-07-22 14:00:13 +0900

    Insertion Sort

---
+++ chapter3/InsertionSort/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Input
+        Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        // Sort
+        insertionSort(a, n);
+    }
+
+    private static void insertionSort(int[] a, int n) {
+        for (int i = 0; i < n; i++) {
+            int v = a[i];
+            int j = i - 1;
+            while (j >= 0 && a[j] > v) { 
+                a[j+1] = a[j];
+                j--;
+            }
+            a[j+1]= v;
+            printArrayInt(a, n);
+        }
+    }
+
+    private static void printArrayInt(int[] a, int n) {
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                System.out.print(" ");
+            }
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    }
+}
